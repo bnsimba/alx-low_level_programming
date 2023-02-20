37c7f8ec2c16fce3d8cd0535d23ba473932ebaad
bilalisimba <bilalingowa36@yahoo.com>
2023-02-20 11:02:42 -0800

    printing a string using puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main-returning zero-zero
+ *
+ * this code prints a string
+ *
+ * Return: 0 if successful
+ */
+int main ()
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
