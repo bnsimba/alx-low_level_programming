ff3dae8686bf68afaf47ed4a861c2ee141e9b646
bilalisimba <bilalingowa36@yahoo.com>
2023-02-20 11:28:12 -0800

    using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main-using printf-zero
+ *
+ * printing a string using printf
+ *
+ * Return: 0 if succesful
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+		return (0);
+}
